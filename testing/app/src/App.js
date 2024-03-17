ba2218836b93177f40412f6c320bd34a910e7ba3
aruzaev <ruzaev.artem21@gmail.com>
2024-03-17 15:02:42 -0400

    Add misc

---
+++ testing/app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fortnite sussy ballklllls</h1>
+
+      <h2>hehehehehjbkjsbd</h2>
+    </div>
+  );
+}
+
+export default App;
